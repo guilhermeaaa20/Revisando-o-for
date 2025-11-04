281d3845aa2a8860a96aa8f74ca99f8fde7a35b6
guilhermeaaa20 <guilhermeaaa20@gmail.com>
2025-11-04 00:38:02 -0300

    Update index.html
    Atividade de 1 a 10 de estrutura de repetição, feita até a 4

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+//Atividade revisando o for
+//Exercício 1:
+
+let np = 10; //np: número pedido
+for (let index=1; index<=np; index+=1) {
+    console.log("Número:"+index);
+};
+    console.log("Feito!");
+
+//Atividade 2:
+    np = 10;    
+for (index=1; index<=np; index+=1) {
+    let soma = np + index;
+    console.log("A soma de: "+np+" com "+index+" resulta em "+soma);
+}; 
+    console.log("Feito!")
+
+//Atividade 3:
+    np = 20;
+for (index=1; index<=np; index+=1) {
+    let pares = (index % 2 == 0) 
+    if (pares) {
+        console.log("Número "+index+" par:"+pares);
+    };
+};
+        console.log("Feito!");
+
+//Atividade 4:
+    np = 10;
+    console.log("A tabuada do número "+np+" é:")
+for (index=1; index<=np; index+=1) {
+    let tabuada = np * index;
+    console.log("Passo "+index+":  "+np+" * "+index+" = "+""+tabuada);
+};
+    console.log("Feito!");
+
+//Atividade 5:
+    np = 5;
+for (index=1; index<=np; index+=1) {
+    let numeros = prompt("Digite 5 números para que seja resolvido a média deles.");
+    
+
+}
